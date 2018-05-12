c1115d819290625a78c91139c58c1740551aae9a
Aleksander Hersztek <aleksander.hersztek@gmail.com>
2018-05-12 15:33:34 +0200

    cleaned  the code

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
 $(document).ready(function(){
     navigator.geolocation.getCurrentPosition(function(location) {
-      var lat;
-      var lon;
-      var myUrl;
+      
       if (location){
-       lat =location.coords.latitude;
-       lon =location.coords.longitude;
-        myUrl="https://fcc-weather-api.glitch.me/api/current?lat="+lat +"&lon="+lon;
-      console.log(myUrl);
-    $.getJSON(myUrl,function(data){
-      var tempFa= (data.main.temp*1.8)+32;
-      $(".temp-view").html(data.main.temp.toFixed(1));
-      $(".city-view").html(data.name);
-      $(".weather-image").attr("src",data.weather[0].icon);
-      $(".change-scale").on("click",function(){
-        console.log($(".change-scale").text());
-        if ($ (".change-scale").text() =="C" ){
-        $(".change-scale").text("F");
-        $(".temp-view").text(tempFa.toFixed(1));
-        }
-        else if ($(".change-scale").text()=="F"){
-          $(".change-scale").text("C");
-          $(".temp-view").text(data.main.temp.toFixed(1));
-        }
-        $(".temp-view").addClass("animated fadeInLeftBig");
-        window.setTimeout(function (){
-         $(".temp-view").removeClass("animated fadeInLeftBig"); 
-        },2000);
-       
-      });
-    });
+        getWeatherData(location);
       }
     });
       
-      $('.change-scale').each(function() {
-            animationHover(this, 'bounceIn',$(".temp-view"));
+      });
+      function getWeatherData(location){
+        var lat;
+        var lon;
+        var myUrl;
+        lat =location.coords.latitude;
+        lon =location.coords.longitude;
+        myUrl="https://fcc-weather-api.glitch.me/api/current?lat="+lat +"&lon="+lon;
+        console.log(myUrl);
+      $.getJSON(myUrl,function(data){
+        var tempFa= (data.main.temp*1.8)+32;
+        $(".temp-view").html(data.main.temp.toFixed(1));
+        $(".city-view").html(data.name);
+        $(".weather-image").attr("src",data.weather[0].icon);
+        $(".change-scale").on("click",function(){
+          console.log($(".change-scale").text());
+          if ($ (".change-scale").text() =="C" ){
+          $(".change-scale").text("F");
+          $(".temp-view").text(tempFa.toFixed(1));
+          }
+          else if ($(".change-scale").text()=="F"){
+            $(".change-scale").text("C");
+            $(".temp-view").text(data.main.temp.toFixed(1));
+          }
         });
       });
-    function animationHover(element, animation ,target){
-        element = $(element);
-        element.hover(
-            function() {
-                target.addClass('animated ' + animation);        
-            },
-            function(){
-                window.setTimeout( function(){
-                    target.removeClass('animated ' + animation);
-                }, 2000);         
-            });
-    }
+      }
